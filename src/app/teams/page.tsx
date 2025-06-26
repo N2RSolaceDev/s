c3ea6854d798d8f951901f4b909f67cc2bf7e471
Solace <monkdev9@gmail.com>
2025-06-26 20:50:31 +0100

    Update page.tsx

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -19,7 +19,7 @@ const teams = [
     name: 'Ownership/Management',
     image: '/logo.png',
     description: 'Void Esports Management and Ownership Team',
-    roster: ['Gruun', 'Frankenstein', 'Bxezy', 'JECI', 'Dixuez', 'Dr Puffin'],
+    roster: ['Gruun', 'Frankenstein', 'Bxezy', 'JECI', 'Dixuez', 'Dr Puffin', 'Solace'],
     achievements: ['The Very Upper Executives of Void Esports'],
   },
 ];
